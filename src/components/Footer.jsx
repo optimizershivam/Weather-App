b61b7619a65c054bee2085547159dbc453dba87f
optimizershivam <itsurshivam9@gmail.com>
2023-04-28 13:13:27 +0530

    weather app

---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import { useContext } from "react";
+import AppContext from "../provider/appContext";
+
+function Footer() {
+  const {
+    app: { isDark },
+  } = useContext(AppContext);
+  const anchorStyle = {
+    color: "#C6C6C6",
+  };
+  return (
+    <>
+      
+    </>
+  );
+}
+
+export default Footer;
